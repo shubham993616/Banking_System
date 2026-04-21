a83e52dc08f0a67abccd364e1074bf463b87a48d
Shubham Modanwal <shubham993615@gmail.com>
2026-04-21 14:24:27 +0530

    Removed hardcoded secrets and moved to environment variables

--- account-service/src/main/java/com/banking/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/banking/accountservice/AccountServiceApplication.java
@@ -2,6 +2,9 @@ package com.banking.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+
+import jakarta.annotation.PostConstruct;
 
 /**
  * Entry point for the Account Service.
@@ -10,8 +13,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class AccountServiceApplication {
+     @Value("${app.jwt.secret:}")
+    private String jwtSecret;
+
+    @Value("${app.admin.email:}")
+    private String adminEmail;
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
+    @PostConstruct
+public void init() {
+    if (jwtSecret == null || jwtSecret.isBlank()) {
+        throw new RuntimeException("JWT secret is missing!");
+    }
+
+    if (adminEmail == null || adminEmail.isBlank()) {
+        throw new RuntimeException("Admin email is missing!");
+    }
+
+    System.out.println("Config loaded successfully");
+}
+
+      
 }
